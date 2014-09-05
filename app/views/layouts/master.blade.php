f1a89cf2195ad7d358170d7c5b81427cbcaaa6e7
Ark Eidolon <clarke.plumo@gmail.com>
2014-09-06 05:01:15 +0800

    Refactor loading screen

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -13,10 +13,17 @@
 		<div ng-show="currLoad == MAX_LOAD">
 			@yield('content')
 		</div>
-		<div ng-hide="currLoad == MAX_LOAD">
-			<div class="progress">
-				<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="@{{currLoad / MAX_LOAD * 100}}" aria-valuemin="0" aria-valuemax="100" style="width: @{{currLoad / MAX_LOAD * 100}}%">
-					<span class="sr-only">@{{currLoad / MAX_LOAD * 100}}% Complete</span>
+		<div ng-hide="currLoad == MAX_LOAD" class="col-md-offset-3 col-md-6">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title">Loading...</h3>
+				</div>
+				<div class="panel-body">
+					<div class="progress">
+						<div class="progress-bar progress-bar-info progress-bar-striped active"  role="progressbar" aria-valuenow="@{{currLoad / MAX_LOAD * 100}}" aria-valuemin="0" aria-valuemax="100" style="width: @{{currLoad / MAX_LOAD * 100}}%">
+							<span class="sr-only">@{{currLoad / MAX_LOAD * 100}}% Complete</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
