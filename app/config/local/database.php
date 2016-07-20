a29fdf109fc713221d3e16eda93b7575d79a2046
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2016-07-20 12:58:28 +0800

    Add Procfile for Heroku

---
+++ app/config/local/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'default' => 'pgsql',
+    'connections' => array(
+        'pgsql' => array(
+                'driver'   => 'pgsql',
+                'host'     => 'localhost',
+                'database' => 'sjs',
+                'username' => 'arkeidolon',
+                'password' => '',
+                'charset'  => 'utf8',
+                'prefix'   => '',
+                'schema'   => 'public',
+        ),
+    ),
+);
